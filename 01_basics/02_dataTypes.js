68c8f54ae917d5e75dd5ab95c87b35e86ccd8692
khojwar <66153890+khojwar@users.noreply.github.com>
2023-01-01 09:47:33 +0545

    Data Type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,42 @@
+    "use strict"  // treat all js code as newer version
+
+    /*
+    it gives error because we are using nodejs, not browser
+    alert("hello")
+    alert(3+3)
+    */  
+
+    // code readability hunparxa. talako code readable bhayana
+    console.log(3
+        +
+        3);    // readablility should be high
+        
+    console.log("Tika Ram");
+
+
+    let name = "Tika"
+    let age = 26
+    let isLoggedIn = true
+
+
+    // daatypes
+
+    // number  => range is 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true / false
+    // null => standalone value
+    // undefined 
+    // symbol => unique
+
+
+
+    // object
+
+    console.log(typeof "Tika Ram")
+    console.log(typeof age);
+
+
+    console.log(typeof null);        // null is object data type
+    console.log(typeof undefined);   // undefined is undefined data type
+
