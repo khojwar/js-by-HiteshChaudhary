a20d1073d9c28bc1b29c60bddb73c6463b4c2798
Tika Ram Khojwar <66153890+khojwar@users.noreply.github.com>
2023-10-07 11:53:36 +0000

    classes

---
+++ 10_classes_and_oop/06_inheritance.js
@@ -0,0 +1,30 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){         // constructor override gareko
+        super(username)         // User class ko username linxa
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.logMe()
+const masalaChai = new User("masalaChai")
+
+masalaChai.logMe()
+
+console.log(chai instanceof User);
